693f79edba9014c25665f0f5205a4e6405ed70c5
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-05-16 17:37:13 -0400

    Inyección del dao

--- app/src/main/java/edu/ucne/registrotecnico/data/local/database/TecnicoDb.kt
+++ app/src/main/java/edu/ucne/registrotecnico/data/local/database/TecnicoDb.kt
@@ -2,16 +2,20 @@ package edu.ucne.registrotecnicos.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.registrotecnico.data.local.dao.PrioridadDao
 import edu.ucne.registrotecnico.data.local.dao.TecnicoDao
+import edu.ucne.registrotecnico.data.local.entities.PrioridadEntity
 import edu.ucne.registrotecnico.data.local.entities.TecnicoEntity
 
 @Database(
     entities = [
-        TecnicoEntity::class
+        TecnicoEntity::class,
+        PrioridadEntity:: class
     ],
     version = 1,
     exportSchema = false
 )
 abstract class TecnicoDb : RoomDatabase() {
     abstract fun TecnicoDao(): TecnicoDao
+    abstract fun PrioridadDao(): PrioridadDao
 }
