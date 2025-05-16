fe4e5a35e9e23d04d9b1b78a68f5f8726f35349d
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-05-16 16:09:16 -0400

    TecnicosViewModel creado

--- app/src/main/java/edu/ucne/registrotecnico/presentation/tecnicos/TecnicosViewModel.kt
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/tecnicos/TecnicosViewModel.kt
@@ -1,4 +1,27 @@
 package edu.ucne.registrotecnico.presentation.tecnicos
 
-class TecnicosViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import edu.ucne.registrotecnico.data.local.entities.TecnicoEntity
+import edu.ucne.registrotecnico.data.repository.TecnicosRepository
+import kotlinx.coroutines.launch
+
+class TecnicosViewModel(
+    private val tecnicosRepository: TecnicosRepository
+) : ViewModel() {
+    fun saveTecnico(tecnico: TecnicoEntity) {
+        viewModelScope.launch {
+            tecnicosRepository.save(tecnico)
+        }
+    }
+
+    suspend fun findTecnico(id: Int): TecnicoEntity? {
+        return tecnicosRepository.find(id)
+    }
+
+    fun deleteTecnico(tecnico: TecnicoEntity) {
+        viewModelScope.launch {
+            tecnicosRepository.delete(tecnico)
+        }
+    }
 }
