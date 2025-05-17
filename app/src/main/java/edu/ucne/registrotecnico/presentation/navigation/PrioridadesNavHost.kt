d30acc9c7892e6f3245e0419f736d2cb233bcc74
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-05-16 20:26:51 -0400

    Implementando navegación

---
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/navigation/PrioridadesNavHost.kt
@@ -0,0 +1,52 @@
+package edu.ucne.registrotecnico.presentation.navigation
+
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import edu.ucne.registrotecnico.data.local.entities.PrioridadEntity
+import edu.ucne.registrotecnico.data.local.entities.TecnicoEntity
+import edu.ucne.registrotecnico.presentation.prioridades.PrioridadListScreen
+import edu.ucne.registrotecnico.presentation.prioridades.PrioridadScreen
+import edu.ucne.registrotecnico.presentation.prioridades.PrioridadesViewModel
+import edu.ucne.registrotecnico.presentation.tecnicos.TecnicoListScreen
+import edu.ucne.registrotecnico.presentation.tecnicos.TecnicoScreen
+import edu.ucne.registrotecnico.presentation.tecnicos.TecnicosViewModel
+
+
+@Composable
+fun PrioridadesNavHost(
+    navHostController: NavHostController,
+    prioridadList: List<PrioridadEntity>,
+    viewModel: PrioridadesViewModel,
+    navController: NavController
+) {
+    NavHost(
+        navController = navHostController,
+        startDestination = Screen.PrioridadList
+    ) {
+        composable<Screen.PrioridadList> {
+            PrioridadListScreen(
+                prioridadList = prioridadList,
+                onEdit = { prioridadId ->
+                    navHostController.navigate(Screen.Prioridad(prioridadId))
+                },
+                onDelete = { prioridad ->
+                    viewModel.deletePrioridad(prioridad)
+                }
+            )
+        }
+
+        composable<Screen.Prioridad>{ backStack ->
+            val prioridadId  = backStack.toRoute<Screen.Prioridad>().prioridadId
+            PrioridadScreen(
+                prioridadId,
+                viewModel,
+                navController
+            )
+        }
+    }
+}
