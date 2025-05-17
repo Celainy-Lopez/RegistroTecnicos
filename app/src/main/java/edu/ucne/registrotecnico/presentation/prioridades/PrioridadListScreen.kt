4c33705866952936d098b9339fad7d45ebf4808c
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-05-16 20:25:22 -0400

    Implementando la vista para la consulta de prioridades

--- app/src/main/java/edu/ucne/registrotecnico/presentation/prioridades/PrioridadListScreen.kt
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/prioridades/PrioridadListScreen.kt
@@ -1,4 +1,114 @@
 package edu.ucne.registrotecnico.presentation.prioridades
 
-class PrioridadListScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import edu.ucne.registrotecnico.data.local.entities.PrioridadEntity
+import edu.ucne.registrotecnico.ui.theme.RegistroTecnicoTheme
+import java.text.DecimalFormat
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PrioridadListScreen(
+    prioridadList: List<PrioridadEntity>,
+    onEdit: (Int?) -> Unit,
+    onDelete: (PrioridadEntity) -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Lista de prioridades") })
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { onEdit(0) }) {
+                Icon(Icons.Filled.Add, "Agregar nueva")
+            }
+        }
+    ) { padding ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(padding)
+        ) {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(prioridadList) { prioridad ->
+                    PrioridadRow(prioridad, { onEdit(prioridad.prioridadId) },
+                        { onDelete(prioridad) })
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun PrioridadRow(
+    prioridad: PrioridadEntity,
+    onEdit: () -> Unit,
+    onDelete: () -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+    ) {
+
+        val decimalFormat = DecimalFormat("#,##0.00")
+        Text(modifier = Modifier.weight(1f), text = prioridad.prioridadId.toString())
+        Text(modifier = Modifier.weight(2f), text = prioridad.descripcion)
+
+        IconButton(onClick = onEdit) {
+            Icon(imageVector = Icons.Default.Edit, contentDescription = "Editar")
+        }
+        IconButton(onClick = onDelete) {
+            Icon(imageVector = Icons.Default.Delete, contentDescription = "Eliminar")
+        }
+    }
+    HorizontalDivider()
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun Preview() {
+    val prioridades = listOf(
+        PrioridadEntity(
+            prioridadId = 1,
+            descripcion = "Alta",
+        ),
+        PrioridadEntity(
+            prioridadId = 2,
+            descripcion = "Alta",
+        )
+    )
+    RegistroTecnicoTheme {
+        PrioridadListScreen(
+            prioridadList = prioridades,
+            onEdit = {},
+            onDelete = {}
+        )
+    }
 }
