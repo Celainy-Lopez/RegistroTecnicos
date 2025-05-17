710156cd3a771a5c8f233595ef65dba1a30bdccd
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-05-16 20:27:30 -0400

    Implementando PrioridadesViewModel

--- app/src/main/java/edu/ucne/registrotecnico/presentation/prioridades/PrioridadesViewModel.kt
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/prioridades/PrioridadesViewModel.kt
@@ -1,4 +1,27 @@
 package edu.ucne.registrotecnico.presentation.prioridades
 
-class PrioridadesViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import edu.ucne.registrotecnico.data.local.entities.PrioridadEntity
+import edu.ucne.registrotecnico.data.repository.PrioridadesRepository
+import kotlinx.coroutines.launch
+
+class PrioridadesViewModel(
+    private val prioridadesRepository: PrioridadesRepository
+) : ViewModel() {
+    fun savePrioridad(prioridad: PrioridadEntity) {
+        viewModelScope.launch {
+            prioridadesRepository.save(prioridad)
+        }
+    }
+
+    suspend fun findPrioridad(id: Int): PrioridadEntity? {
+        return prioridadesRepository.find(id)
+    }
+
+    fun deletePrioridad(prioridad: PrioridadEntity) {
+        viewModelScope.launch {
+            prioridadesRepository.delete(prioridad)
+        }
+    }
 }
