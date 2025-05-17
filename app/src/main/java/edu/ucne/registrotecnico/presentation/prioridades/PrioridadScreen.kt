0fafd9810f2dfb7a84df52c3a67c7aed723ba5ab
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-05-16 20:23:48 -0400

    Implementando la vista para la creación de prioridades

--- app/src/main/java/edu/ucne/registrotecnico/presentation/prioridades/PrioridadScreen.kt
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/prioridades/PrioridadScreen.kt
@@ -1,4 +1,166 @@
 package edu.ucne.registrotecnico.presentation.prioridades
 
-class PrioridadScreen {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import edu.ucne.registrotecnico.data.local.entities.PrioridadEntity
+
+@Composable
+fun PrioridadScreen(
+    prioridadId: Int? = null,
+    viewModel: PrioridadesViewModel,
+    navController: NavController,
+) {
+    var descripcion by remember { mutableStateOf("") }
+    var errorMessage: String? by remember { mutableStateOf(null) }
+    var existe by remember { mutableStateOf<PrioridadEntity?>(null) }
+
+    LaunchedEffect(prioridadId) {
+        if (prioridadId != null && prioridadId > 0) {
+            val prioridad = viewModel.findPrioridad(prioridadId)
+            prioridad?.let {
+                existe = it
+                descripcion = it.descripcion
+            }
+        }
+    }
+
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                if (navController != null) {
+                    IconButton(
+                        onClick = { navController.popBackStack() },
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    ) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "volver")
+                    }
+                }
+            }
+            ElevatedCard(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    Text(
+                        text = "Registro prioridad",
+                        style = MaterialTheme.typography.headlineMedium,
+                        color = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+
+                    Spacer(modifier = Modifier.height(32.dp))
+                    OutlinedTextField(
+                        value = prioridadId.toString() ?: "0",
+                        onValueChange = {},
+                        label = { Text("ID") },
+                        modifier = Modifier.fillMaxWidth(),
+                        readOnly = true,
+                        enabled = false
+                    )
+
+                    OutlinedTextField(
+                        value = descripcion,
+                        onValueChange = { descripcion = it },
+                        label = { Text("Descripción") },
+                        placeholder = { Text("Ej: Alta") },
+                        modifier = Modifier.fillMaxWidth(),
+                        singleLine = true
+                    )
+
+                    Spacer(modifier = Modifier.padding(2.dp))
+                    errorMessage?.let {
+                        Text(text = it, color = Color.Red)
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        OutlinedButton(
+                            onClick = {
+                                descripcion = ""
+                                errorMessage = null
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Add,
+                                contentDescription = "new button"
+                            )
+                            Text(text = "Nuevo")
+                        }
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        OutlinedButton(
+                            onClick = {
+                                if (descripcion.isBlank()) {
+                                    errorMessage = "Descripción vacia."
+                                    return@OutlinedButton
+                                }
+
+                                viewModel.savePrioridad(
+                                    PrioridadEntity(
+                                        prioridadId = existe?.prioridadId,
+                                        descripcion = descripcion,
+                                    )
+                                )
+                            }
+                        )
+
+                        {
+                            Icon(
+                                imageVector = Icons.Default.Edit,
+                                contentDescription = "save button"
+                            )
+                            Text(text = "Guardar")
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
+
