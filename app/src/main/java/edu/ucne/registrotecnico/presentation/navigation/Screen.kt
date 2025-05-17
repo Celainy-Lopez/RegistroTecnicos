95fbf62015f7faaf924ae2feb21c64ebc28ca80c
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-05-16 20:21:23 -0400

    Aplicando serialización

--- app/src/main/java/edu/ucne/registrotecnico/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/navigation/Screen.kt
@@ -6,5 +6,10 @@ sealed class Screen {
     @Serializable
     data object TecnicoList : Screen()
     @Serializable
+    data object PrioridadList: Screen()
+
+    @Serializable
     data class Tecnico(val tecnicoId: Int?) : Screen()
+    @Serializable
+    data class Prioridad(val prioridadId: Int?) : Screen()
 }
