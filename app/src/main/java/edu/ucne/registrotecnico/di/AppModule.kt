e47ff3cb1c7dc7054615356f27c80f88f9eb69a2
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-05-25 04:31:26 -0400

    Añadiendo mensaje al modelo de inyección de dependencias

--- app/src/main/java/edu/ucne/registrotecnico/di/AppModule.kt
+++ app/src/main/java/edu/ucne/registrotecnico/di/AppModule.kt
@@ -29,4 +29,6 @@ object AppModule {
     fun providePrioridadDao(tecnicoDb: TecnicoDb) = tecnicoDb.PrioridadDao()
     @Provides
     fun provideTicketDao(tecnicoDb: TecnicoDb) = tecnicoDb.TicketDao()
+    @Provides
+    fun provideMensajeDao(tecnicoDb: TecnicoDb) = tecnicoDb.MensajeDao()
 }
