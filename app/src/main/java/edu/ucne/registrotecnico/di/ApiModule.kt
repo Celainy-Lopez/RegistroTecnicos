815e67e5d3e329b8bf4add71d9c98f9748efb76c
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-15 22:57:29 -0400

    Agregando URL del servicio web, Moshi y convertidor

--- app/src/main/java/edu/ucne/registrotecnico/di/ApiModule.kt
+++ app/src/main/java/edu/ucne/registrotecnico/di/ApiModule.kt
@@ -1,4 +1,36 @@
 package edu.ucne.registrotecnico.di
 
-class ApiModule {
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.registrotecnico.data.remote.SistemainApi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+import kotlin.jvm.java
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ApiModule {
+    const val BASE_URL = "https://registrosistemaapi.azurewebsites.net"
+
+    @Provides
+    @Singleton
+    fun providesMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory()).add(DateAdapter())
+            .build()
+
+    @Provides
+    @Singleton
+    fun providesSistemasApi(moshi: Moshi): SistemainApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(SistemainApi::class.java)
+    }
 }
