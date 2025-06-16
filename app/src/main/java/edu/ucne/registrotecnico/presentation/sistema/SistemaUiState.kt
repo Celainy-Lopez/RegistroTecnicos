a2b05195e1d82cb47c7a2cbcaec42db84cc7c569
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-16 17:12:57 -0400

    Capa de usuario creada(UiState)

--- app/src/main/java/edu/ucne/registrotecnico/presentation/sistema/SistemaUiState.kt
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/sistema/SistemaUiState.kt
@@ -1,4 +1,21 @@
 package edu.ucne.registrotecnico.presentation.sistema
 
-class SistemaUiState {
-}
+import edu.ucne.registrotecnico.data.remote.dto.SistemaDto
+
+data class SistemaUiState(
+    val sistemaId: Int? = null,
+    val nombre: String = "",
+    val descripcion: String = "",
+    val costo: Double = 0.0,
+
+    val errorMessage: String? = null,
+    val errorNombre: String? = null,
+    val errorDesripcion: String? = null,
+    val errorCosto: String? = null,
+
+    val successMessage: String? = null,
+    val isSuccess: Boolean = false,
+    val isLoading: Boolean = false,
+
+    val sistemas: List<SistemaDto> = emptyList()
+)
