faf8504e01e3eb5c0b440185847386e6b6086378
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-15 22:50:30 -0400

    interfaz SistemainApi Creada

--- app/src/main/java/edu/ucne/registrotecnico/data/remote/SistemainApi.kt
+++ app/src/main/java/edu/ucne/registrotecnico/data/remote/SistemainApi.kt
@@ -1,4 +1,27 @@
 package edu.ucne.registrotecnico.data.remote
 
-class SistemainApi {
+import edu.ucne.registrotecnico.data.remote.dto.SistemaDto
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface SistemainApi {
+    @GET("api/Sistemas")
+    suspend fun getSistemas(): List<SistemaDto>
+
+    @GET("api/Sistemas/{id}")
+    suspend fun getSistema(@Path("id") id: Int): List<SistemaDto>
+
+    @PUT("api/Sistemas/{id}")
+    suspend fun updateSistema(@Body sistemaDto: SistemaDto): SistemaDto
+
+    @POST("api/Sistemas")
+    suspend fun saveSistema(@Body sistemaDto: SistemaDto): SistemaDto
+
+    @DELETE("api/Sistemas/{id}")
+    suspend fun deleteSistema(@Path("id") id: Int): Response<Unit>
 }
