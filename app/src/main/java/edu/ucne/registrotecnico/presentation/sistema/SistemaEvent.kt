578b6cb472f95a8c7b4fb34a7ff8a62b90353767
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-16 17:13:38 -0400

    Eventos creados

--- app/src/main/java/edu/ucne/registrotecnico/presentation/sistema/SistemaEvent.kt
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/sistema/SistemaEvent.kt
@@ -1,4 +1,17 @@
 package edu.ucne.registrotecnico.presentation.sistema
 
-interface SistemaEvent {
+sealed interface SistemaEvent {
+    data class SistemaChange(val usuarioId: Int): SistemaEvent
+    data class NombreChange(val nombre: String): SistemaEvent
+    data class DescripcionChange(val descripcion: String): SistemaEvent
+    data class CostoChange(val costo: Double ): SistemaEvent
+
+    data object GetSistemas: SistemaEvent
+    data object PostSistema: SistemaEvent
+    data object Nuevo: SistemaEvent
+    data class GetSistema(val id: Int): SistemaEvent
+
+    data object CleanErrorMessageNombre: SistemaEvent
+    data object CleanErrorMessageBalance: SistemaEvent
+    data object ResetSuccessMessage: SistemaEvent
 }
