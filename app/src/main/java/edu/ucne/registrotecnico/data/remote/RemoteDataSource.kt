3b77df63fb2a42bdc974e6906d5f921d39bb6636
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-15 22:51:04 -0400

    RemoteDataSource creado

--- app/src/main/java/edu/ucne/registrotecnico/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/registrotecnico/data/remote/RemoteDataSource.kt
@@ -1,4 +1,18 @@
 package edu.ucne.registrotecnico.data.remote
 
-class RemoteDataSource {
+import edu.ucne.registrotecnico.data.remote.dto.SistemaDto
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val sistemainApi: SistemainApi
+){
+    suspend fun getSistemas()= sistemainApi.getSistemas()
+
+    suspend fun updateSistema(sistemaDto: SistemaDto)= sistemainApi.updateSistema(sistemaDto)
+
+    suspend fun saveSistema(sistemaDto: SistemaDto)= sistemainApi.saveSistema(sistemaDto)
+
+    suspend fun deleteSistema(id: Int)= sistemainApi.deleteSistema(id)
+
+    suspend fun getSistema(id: Int)= sistemainApi.getSistema(id)
 }
