b6f434fb7f443b479724ff309be6ef76df77133a
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-07-23 18:20:48 -0400

    Entidad y Dao agregada

--- app/src/main/java/edu/ucne/registrotecnico/data/local/database/TecnicoDb.kt
+++ app/src/main/java/edu/ucne/registrotecnico/data/local/database/TecnicoDb.kt
@@ -6,12 +6,16 @@ import androidx.room.TypeConverter
 import androidx.room.TypeConverters
 import edu.ucne.registrotecnico.data.local.dao.MensajeDao
 import edu.ucne.registrotecnico.data.local.dao.PrioridadDao
+import edu.ucne.registrotecnico.data.local.dao.SistemaDao
 import edu.ucne.registrotecnico.data.local.dao.TecnicoDao
 import edu.ucne.registrotecnico.data.local.dao.TicketDao
+import edu.ucne.registrotecnico.data.local.dao.UsuarioDao
 import edu.ucne.registrotecnico.data.local.entities.MensajeEntity
 import edu.ucne.registrotecnico.data.local.entities.PrioridadEntity
+import edu.ucne.registrotecnico.data.local.entities.SistemaEntity
 import edu.ucne.registrotecnico.data.local.entities.TecnicoEntity
 import edu.ucne.registrotecnico.data.local.entities.TicketEntity
+import edu.ucne.registrotecnico.data.local.entities.UsuarioEntity
 import java.util.Date
 
 
@@ -32,9 +36,11 @@ class Converters {
         TecnicoEntity::class,
         PrioridadEntity:: class,
         TicketEntity:: class,
-        MensajeEntity:: class
+        MensajeEntity:: class,
+        SistemaEntity:: class,
+        UsuarioEntity::class
     ],
-    version = 6,
+    version = 8,
     exportSchema = false
 )
 
@@ -44,4 +50,6 @@ abstract class TecnicoDb : RoomDatabase() {
     abstract fun PrioridadDao(): PrioridadDao
     abstract fun TicketDao(): TicketDao
     abstract fun MensajeDao() : MensajeDao
+    abstract fun SistemaDao(): SistemaDao
+    abstract fun UsuarioDao(): UsuarioDao
 }
