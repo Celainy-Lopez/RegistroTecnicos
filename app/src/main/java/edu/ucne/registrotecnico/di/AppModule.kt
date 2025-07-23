2ce5666c6b0a0622d8d31a2a04babc6c781bf8b5
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-07-23 18:32:10 -0400

    Inyectando dependencia

--- app/src/main/java/edu/ucne/registrotecnico/di/AppModule.kt
+++ app/src/main/java/edu/ucne/registrotecnico/di/AppModule.kt
@@ -31,4 +31,8 @@ object AppModule {
     fun provideTicketDao(tecnicoDb: TecnicoDb) = tecnicoDb.TicketDao()
     @Provides
     fun provideMensajeDao(tecnicoDb: TecnicoDb) = tecnicoDb.MensajeDao()
+    @Provides
+    fun provideSistemaDao(tecnicoDb: TecnicoDb) = tecnicoDb.SistemaDao()
+    @Provides
+    fun provideUsuarioDao(tecnicoDb: TecnicoDb) = tecnicoDb.UsuarioDao()
 }
