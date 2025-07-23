c3a704f1aaa245ae12fb031febae750462af7bbf
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-07-23 19:25:51 -0400

    UiState creado

---
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/usuarios/UsuarioUiState.kt
@@ -0,0 +1,20 @@
+package edu.ucne.registrotecnico.presentation.usuarios
+
+import edu.ucne.registrotecnico.data.remote.dto.UsuarioDto
+
+
+data class UsuarioUiState(
+    val usuarioId: Int? = null,
+    val nombre: String = "",
+    val balance: Double = 0.0,
+
+    val errorMessage: String? = null,
+    val errorNombre: String? = null,
+    val errorBalance: String? = null,
+
+    val successMessage: String? = null,
+    val isSuccess: Boolean = false,
+    val isLoading: Boolean = false,
+
+    val usuarios: List<UsuarioDto> = emptyList()
+)
