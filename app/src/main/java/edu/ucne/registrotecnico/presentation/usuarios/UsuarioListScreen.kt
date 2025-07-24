1bd81a76938bb983165a4baea6fe83cba6634332
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-07-23 22:10:07 -0400

    Usuario List Screen creado

---
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/usuarios/UsuarioListScreen.kt
@@ -0,0 +1,234 @@
+package edu.ucne.registrotecnico.presentation.usuarios
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults.cardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import edu.ucne.registrotecnico.data.remote.dto.UsuarioDto
+import edu.ucne.registrotecnico.ui.theme.RegistroTecnicoTheme
+import java.text.DecimalFormat
+
+@Composable
+fun UsuarioListScreen(
+    viewModel: UsuariosViewModel = hiltViewModel(),
+    createUsuario: () -> Unit,
+    goToUsuario: (Int) -> Unit,
+    goBack: () -> Unit
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    UsuarioListBodyScreen(
+        uiState = uiState,
+        goToUsuario = { id -> goToUsuario(id) },
+        createUsuario = createUsuario,
+        onEvent = viewModel::onEvent,
+        goBack = goBack
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
+@Composable
+fun UsuarioListBodyScreen(
+    uiState: UsuarioUiState,
+    goToUsuario: (Int) -> Unit,
+    createUsuario: () -> Unit,
+    onEvent: (UsuarioEvent) -> Unit,
+    goBack: () -> Unit
+) {
+    val refreshing = uiState.isLoading
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = refreshing,
+        onRefresh = { onEvent(UsuarioEvent.GetUsuarios) }
+    )
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        "API Usuarios",
+                        style = MaterialTheme.typography.titleLarge.copy(color = MaterialTheme.colorScheme.surface)
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = goBack) {
+                        Icon(
+                            imageVector =  Icons.Default.ArrowBack,
+                            contentDescription = "Back",
+                            tint = MaterialTheme.colorScheme.surface
+                        )
+                    }
+                },
+
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = createUsuario) {
+                Icon(Icons.Filled.Add, "Agregar")
+            }
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .pullRefresh(pullRefreshState)
+                .padding(innerPadding)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                if (uiState.usuarios.isEmpty()) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(24.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "No hay usuarios registrados",
+                            style = MaterialTheme.typography.bodyLarge,
+                            color = Color.Gray
+                        )
+                    }
+                }  else {
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        items(uiState.usuarios) { usuario ->
+                            UsuarioRow (
+                                it = usuario,
+                                goToUsuario = { goToUsuario(usuario.usuarioId ?: 0) }
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                        }
+                    }
+                }
+
+                if (!uiState.errorMessage.isNullOrEmpty()) {
+                    Row(
+                        modifier = Modifier
+                            .padding(16.dp)
+                    ) {
+                        Text(
+                            text = uiState.errorMessage,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+            }
+
+            PullRefreshIndicator(
+                refreshing = refreshing,
+                state = pullRefreshState,
+                modifier = Modifier.align(Alignment.TopCenter)
+            )
+        }
+    }
+}
+
+@Composable
+private fun UsuarioRow(
+    it: UsuarioDto,
+    goToUsuario: () -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        elevation = cardElevation(6.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            val decimalFormat = DecimalFormat("#,##0.00")
+
+            Row {
+                Text(text = "Usuario #: ", fontWeight = FontWeight.ExtraBold)
+                Text(text = it.usuarioId.toString())
+            }
+
+            Spacer(modifier = Modifier.weight(2f))
+
+            Row {
+                Text(text = "Nombre ", fontWeight = FontWeight.ExtraBold)
+                Text( it.nombre)
+            }
+
+            Spacer(modifier = Modifier.weight(2f))
+
+            Row {
+                Text(text = "Balance: ", fontWeight = FontWeight.ExtraBold)
+                Text(decimalFormat.format(it.balance))
+            }
+        }
+    }
+    HorizontalDivider()
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun Preview() {
+    val usuarios = listOf(
+        UsuarioDto(
+            usuarioId = 1,
+            nombre = "Wilmer Castillo",
+            balance = 10000.0
+        ),
+        UsuarioDto(
+            usuarioId = 2,
+            nombre = "Jose del Orbe",
+            balance = 10000.0
+        )
+    )
+    RegistroTecnicoTheme {
+        UsuarioListScreen(
+            goToUsuario = {},
+            createUsuario = {},
+            goBack = {}
+        )
+    }
+}
