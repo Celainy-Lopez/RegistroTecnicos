7dede1e2c8758303f415a0bc3dd6795bad4c978b
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-08-03 08:30:32 -0400

    Actualizando main activity

--- app/src/main/java/edu/ucne/registrotecnico/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrotecnico/MainActivity.kt
@@ -8,60 +8,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.LocalLifecycleOwner
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
-import androidx.room.Room
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.registrotecnico.data.local.entities.TicketEntity
-import edu.ucne.registrotecnicos.data.local.database.TecnicoDb
-import edu.ucne.registrotecnico.data.repository.PrioridadesRepository
-import edu.ucne.registrotecnico.data.repository.TecnicosRepository
-import edu.ucne.registrotecnico.data.repository.TicketsRepository
 import edu.ucne.registrotecnico.presentation.HomeNavHost
-import edu.ucne.registrotecnico.presentation.prioridades.PrioridadesViewModel
-import edu.ucne.registrotecnico.presentation.tecnicos.TecnicosViewModel
-import edu.ucne.registrotecnico.presentation.tickets.TicketsViewModel
 import edu.ucne.registrotecnico.ui.theme.RegistroTecnicoTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private lateinit var tecnicoDb: TecnicoDb
-    private lateinit var tecnicosRepository: TecnicosRepository
-    private lateinit var tecnicosViewModel: TecnicosViewModel
-
-    private lateinit var prioridadesRepository: PrioridadesRepository
-    private lateinit var prioridadViewModel: PrioridadesViewModel
-
-    private lateinit var ticketsRepository: TicketsRepository
-    private lateinit var ticketsViewModel: TicketsViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        tecnicoDb = Room.databaseBuilder(
-            applicationContext,
-            TecnicoDb::class.java,
-            "Tecnico.db"
-        ).fallbackToDestructiveMigration()
-            .build()
-
-        tecnicosRepository = TecnicosRepository(tecnicoDb.TecnicoDao())
-        tecnicosViewModel = TecnicosViewModel(tecnicosRepository)
-
-        prioridadesRepository = PrioridadesRepository(tecnicoDb.PrioridadDao())
-        prioridadViewModel = PrioridadesViewModel(prioridadesRepository)
-
-        ticketsRepository = TicketsRepository(tecnicoDb.TicketDao())
-        ticketsViewModel = TicketsViewModel(
-            ticketsRepository,
-            tecnicosRepository,
-            prioridadesRepository
-        )
-
         setContent {
             RegistroTecnicoTheme {
                 val nav = rememberNavController()
